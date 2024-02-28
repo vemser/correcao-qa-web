187f5f75841717e4ee19bb47b6a5a835cd8b0bbb
Italo Lacerda <italo.lacerda2017@hotmail.com>
2024-02-27 21:28:58 -0300

    chore: Cria InicialSpecs e ConfigProperties

---
+++ src/main/java/com/vemser/correcao/specs/InicialSpecs.java
@@ -0,0 +1,25 @@
+package com.vemser.correcao.specs;
+
+import com.vemser.correcao.utils.ConfigProperties;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.config;
+import static io.restassured.config.LogConfig.logConfig;
+
+public class InicialSpecs {
+
+    private InicialSpecs() {}
+
+    public static RequestSpecification setup() {
+        ConfigProperties.initializePropertyFile();
+
+        return new RequestSpecBuilder()
+                .setBaseUri(ConfigProperties.properties.getProperty("AppURL"))
+                .setPort(Integer.parseInt(ConfigProperties.properties.getProperty("Port")))
+                .setConfig(config().logConfig(
+                        logConfig().enableLoggingOfRequestAndResponseIfValidationFails()
+                ))
+                .build();
+    }
+}
---
+++ src/main/java/com/vemser/correcao/utils/ConfigProperties.java
@@ -0,0 +1,23 @@
+package com.vemser.correcao.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigProperties {
+    public static Properties properties;
+
+    public static void initializePropertyFile(){
+        properties = new Properties();
+        try {
+            String configPath = "src/main/resources/configsetting.properties";
+            InputStream instm = new FileInputStream(configPath);
+            properties.load(instm);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
