ee524cc508e2d9963ad392e2261b69a2777afb3c
Italo Lacerda <italo.lacerda2017@hotmail.com>
2024-03-01 14:44:36 -0300

    feat: Inicia Testes Do Get De Questao

--- src/main/java/com/vemser/correcao/client/QuestaoClient.java
+++ src/main/java/com/vemser/correcao/client/QuestaoClient.java
@@ -4,11 +4,15 @@ import com.vemser.correcao.dto.QuestaoDto;
 import com.vemser.correcao.specs.QuestaoSpecs;
 import io.restassured.response.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 
 public class QuestaoClient {
 
     private static final String QUESTAO = "/questao/criar-questao-com-testes";
+    private static final String LISTAR_QUESTAO_URL = "/questao/listar-todas-questoes";
 
     public QuestaoClient() {
     }
@@ -20,4 +24,18 @@ public class QuestaoClient {
                     .post(QUESTAO);
     }
 
+    public static Response buscarTodasQuestao() {
+        Map<String, String> parametrosMap = new HashMap<>();
+        parametrosMap.put("paginaSolicitada", "0");
+        parametrosMap.put("tamanhoPagina", "10");
+
+        return
+                given()
+                    .spec(QuestaoSpecs.questaoReqSpec())
+                    .auth("")
+                    .pathParams(parametrosMap)
+                        .log().all()
+                .when()
+                    .get(LISTAR_QUESTAO_URL);
+    }
 }
---
+++ src/test/java/com/vemser/correcao/test/funcional/questoes/QuestaoGetFuncionalTest.java
@@ -0,0 +1,31 @@
+package com.vemser.correcao.test.funcional.questoes;
+
+import com.vemser.correcao.client.QuestaoClient;
+import com.vemser.correcao.dto.QuestaoDto;
+import com.vemser.correcao.dto.QuestaoResponseDto;
+import com.vemser.correcao.factory.QuestaoDataFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuestaoGetFuncionalTest {
+    @Test
+    @DisplayName("Questoes - Criar Questao Informando Campos Válidos (Espera Sucesso)")
+    public void testQuestoes_cadastroComDadosValidos_esperaSucesso() {
+        QuestaoResponseDto questaoResult = QuestaoClient.buscarTodasQuestao()
+                .then()
+                    .statusCode(201)
+                    .extract()
+                    .as(QuestaoResponseDto.class);
+
+//        assertAll("Testes de criar questões com sucesso",
+//                () -> assertNotNull(questaoResult.getQuestaoDTO().getQuestaoId(), "Id da questão não deve ser nulo"),
+//                () -> assertEquals(questao.getTitulo(), questaoResult.getQuestaoDTO().getTitulo(), "Título da questão deve ser igual"),
+//                () -> assertEquals(questao.getDificuldade(), questaoResult.getQuestaoDTO().getDificuldade(), "Dificuldade da questão deve ser igual"),
+//                () -> assertEquals(questao.getLinguagem(), questaoResult.getQuestaoDTO().getLinguagem(), "Linguagem da questão deve ser igual"),
+//                () -> assertEquals(questao.getTestes().size(), questaoResult.getQuestaoDTO().getTestes().size(), "Quantidade de testes da questão deve ser igual")
+//        );
+    }
+
+}
