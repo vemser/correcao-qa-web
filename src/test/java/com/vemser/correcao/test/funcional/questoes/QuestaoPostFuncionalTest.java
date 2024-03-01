7bea9a18ce710bd690f67d44e692331136e61f0d
Italo Lacerda <italo.lacerda2017@hotmail.com>
2024-03-01 12:53:55 -0300

    refactor: atualizar estrutura de pacotes

--- src/test/java/com/vemser/correcao/test/funcional/questoes/QuestaoPostFuncionalTest.java
+++ src/test/java/com/vemser/correcao/test/funcional/questoes/QuestaoPostFuncionalTest.java
@@ -1,16 +1,15 @@
-package com.vemser.correcao.test.funcional;
+package com.vemser.correcao.test.funcional.questoes;
 
 import com.vemser.correcao.client.QuestaoClient;
 import com.vemser.correcao.dto.QuestaoDto;
 import com.vemser.correcao.dto.QuestaoResponseDto;
-import com.vemser.correcao.enums.Dificuldade;
 import com.vemser.correcao.factory.QuestaoDataFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class QuestaoFuncionalTest {
+public class QuestaoPostFuncionalTest {
     @Test
     @DisplayName("Questoes - Criar Questao Informando Campos Válidos (Espera Sucesso)")
     public void testQuestoes_cadastroComDadosValidos_esperaSucesso() {
