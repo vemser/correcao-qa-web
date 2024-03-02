675508e437ead17ce217dcfbb5489e2c86f6cc69
Italo Lacerda <italo.lacerda2017@hotmail.com>
2024-03-01 22:23:15 -0300

    feat: cria casos de falha de listar questão

--- src/test/java/com/vemser/correcao/test/funcional/QuestaoGetFuncionalTest.java
+++ src/test/java/com/vemser/correcao/test/funcional/QuestaoGetFuncionalTest.java
@@ -10,11 +10,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class QuestaoGetFuncionalTest {
     @Test
     @DisplayName("Questoes - Listar Todas Questões (Espera Sucesso)")
-    public void testQuestoes_cadastroComDadosValidos_esperaSucesso() {
+    public void testQuestoes_listarTodasQuestoes_esperaSucesso() {
         ListaTodasQuestaoResponseDto questaoResult = QuestaoClient.buscarTodasQuestao("0", "10")
                 .then()
                     .statusCode(200)
-                .log().all()
                     .extract()
                     .as(ListaTodasQuestaoResponseDto.class);
 
@@ -23,6 +22,44 @@ public class QuestaoGetFuncionalTest {
                 () -> assertEquals(questaoResult.getContent().size(), questaoResult.getNumberOfElements())
         );
         assertEquals(questaoResult.getPageable().getPageNumber(), 0,"Verifica se retorna pagina correta");
+    }
+
+    @Test
+    @DisplayName("Questoes - Buscar por uma pagina que não existe (Espera Falha)")
+    public void testQuestoes_buscarPorUmaPaginaQueNaoExiste_esperaFalha() {
+        ListaTodasQuestaoResponseDto questao = QuestaoClient.buscarTodasQuestao("0", "10")
+                                                                    .as(ListaTodasQuestaoResponseDto.class);
+
+        String paginaSolicitada = Integer.toString(questao.getPageable().getPageNumber() + 1);
+
+        ListaTodasQuestaoResponseDto questaoResult = QuestaoClient.buscarTodasQuestao(paginaSolicitada, "10")
+                .then()
+                    .statusCode(404)
+                    .extract()
+                    .as(ListaTodasQuestaoResponseDto.class);
+
+        //        TODO: Definir padrão de error para retorno
+        assertAll("Verifica se retorna lista com tamnaho correto",
+                () -> assertEquals(questaoResult.getNumberOfElements(), 0),
+                () -> assertEquals(questaoResult.getContent().size(), questaoResult.getNumberOfElements())
+        );
+        assertEquals(questaoResult.getPageable().getPageNumber(), 0,"Verifica se retorna pagina correta");
+    }
+
+    @Test
+    @DisplayName("Questoes - Buscar por uma pagina que não existe (Espera Falha)")
+    public void testQuestoes_buscarPorUmaPaginaComValorNegativo_esperaFalha() {
+        ListaTodasQuestaoResponseDto questaoResult = QuestaoClient.buscarTodasQuestao("-1", "10")
+                .then()
+                    .statusCode(404)
+                    .extract()
+                    .as(ListaTodasQuestaoResponseDto.class);
+
+        //        TODO: Definir padrão de error para retorno
+        assertAll("Verifica se retorna lista com tamnaho correto",
+                () -> assertEquals(questaoResult.getNumberOfElements(), 0),
+                () -> assertEquals(questaoResult.getContent().size(), questaoResult.getNumberOfElements())
+        );
     }
 
 }
