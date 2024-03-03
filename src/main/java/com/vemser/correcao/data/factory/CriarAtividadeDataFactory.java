0699b42d7e5347d7be0db9ac2a73c3fbbc7febd4
Luh Santos <luisasasilva19@gmail.com>
2024-03-03 20:54:05 -0300

    refac: refatoração CriarAtividadeDataFactory para criar questões e receber o id dinâmico

--- src/main/java/com/vemser/correcao/data/factory/CriarAtividadeDataFactory.java
+++ src/main/java/com/vemser/correcao/data/factory/CriarAtividadeDataFactory.java
@@ -1,28 +1,39 @@
 package com.vemser.correcao.data.factory;
 
+import com.vemser.correcao.client.QuestaoClient;
 import com.vemser.correcao.dto.CriarAtividadeDto;
+import com.vemser.correcao.dto.QuestaoDto;
+import com.vemser.correcao.dto.QuestaoResponseDto;
 import com.vemser.correcao.enums.Trilha;
 import net.datafaker.Faker;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class CriarAtividadeDataFactory {
 
     private static Faker faker = new Faker(new Locale("PT-BR"));
-    private static final  int QUESTAO_UM_VALIDA = 634;
-    private static final  int QUESTAO_DOIS_VALIDA = 631;
+
     private static final  int QUESTAO_INATIVA = 1;
+
+    public static ArrayList<Integer> listaIdDuasQuestoesValidasParaAtividade() {
+        ArrayList<Integer> questoes = new ArrayList<>();
+        QuestaoDto questao = QuestaoDataFactory.questaoDadosValidos(0);
+        QuestaoResponseDto questaoResult = QuestaoClient.cadastrarQuestao(questao).then()
+                .statusCode(201)
+                .extract().as(QuestaoResponseDto.class);
+        questoes.add(questaoResult.getQuestaoDTO().getQuestaoId());
+
+        return questoes;
+    }
 
     public static CriarAtividadeDto atividadeComDadosValidos() {
         CriarAtividadeDto criarAtividadeValida = new CriarAtividadeDto();
-        ArrayList<Integer> questoes = new ArrayList<>();
-        questoes.add(QUESTAO_UM_VALIDA);
-        questoes.add(QUESTAO_DOIS_VALIDA);
 
         criarAtividadeValida.setDescricao(faker.lorem().paragraph());
         criarAtividadeValida.setEdicaoVemSer("13");
         criarAtividadeValida.setPrazoEntrega("2025-03-01T13:01:41.065Z");
-        criarAtividadeValida.setQuestoes(questoes);
+        criarAtividadeValida.setQuestoes(listaIdDuasQuestoesValidasParaAtividade());
         criarAtividadeValida.setTitulo(faker.book().title());
         criarAtividadeValida.setTrilha(Trilha.BACK);
 
@@ -46,14 +57,11 @@ public class CriarAtividadeDataFactory {
 
     public static CriarAtividadeDto atividadeComDataInvalida() {
         CriarAtividadeDto criarAtividadeValida = new CriarAtividadeDto();
-        ArrayList<Integer> questoes = new ArrayList<>();
-        questoes.add(QUESTAO_UM_VALIDA);
-        questoes.add(QUESTAO_DOIS_VALIDA);
 
         criarAtividadeValida.setDescricao(faker.lorem().paragraph());
         criarAtividadeValida.setEdicaoVemSer("13");
         criarAtividadeValida.setPrazoEntrega("2022-03-01T13:01:41.065Z");
-        criarAtividadeValida.setQuestoes(questoes);
+        criarAtividadeValida.setQuestoes(listaIdDuasQuestoesValidasParaAtividade());
         criarAtividadeValida.setTitulo(faker.book().title());
         criarAtividadeValida.setTrilha(Trilha.BACK);
 
@@ -76,14 +84,11 @@ public class CriarAtividadeDataFactory {
 
     public static CriarAtividadeDto atividadeSemPreencherTitulo() {
         CriarAtividadeDto criarAtividadeValida = new CriarAtividadeDto();
-        ArrayList<Integer> questoes = new ArrayList<>();
-        questoes.add(QUESTAO_UM_VALIDA);
-        questoes.add(QUESTAO_DOIS_VALIDA);
 
         criarAtividadeValida.setDescricao(faker.lorem().paragraph());
         criarAtividadeValida.setEdicaoVemSer("13");
         criarAtividadeValida.setPrazoEntrega("2025-03-01T13:01:41.065Z");
-        criarAtividadeValida.setQuestoes(questoes);
+        criarAtividadeValida.setQuestoes(listaIdDuasQuestoesValidasParaAtividade());
         criarAtividadeValida.setTitulo("");
         criarAtividadeValida.setTrilha(Trilha.BACK);
 
@@ -92,14 +97,11 @@ public class CriarAtividadeDataFactory {
 
     public static CriarAtividadeDto atividadeSemPreencherDescricao() {
         CriarAtividadeDto criarAtividadeValida = new CriarAtividadeDto();
-        ArrayList<Integer> questoes = new ArrayList<>();
-        questoes.add(QUESTAO_UM_VALIDA);
-        questoes.add(QUESTAO_DOIS_VALIDA);
 
         criarAtividadeValida.setDescricao("");
         criarAtividadeValida.setEdicaoVemSer("13");
         criarAtividadeValida.setPrazoEntrega("2025-03-01T13:01:41.065Z");
-        criarAtividadeValida.setQuestoes(questoes);
+        criarAtividadeValida.setQuestoes(listaIdDuasQuestoesValidasParaAtividade());
         criarAtividadeValida.setTitulo(faker.book().title());
         criarAtividadeValida.setTrilha(Trilha.BACK);
 
@@ -108,14 +110,11 @@ public class CriarAtividadeDataFactory {
 
     public static CriarAtividadeDto atividadeSemPreencherEdicao() {
         CriarAtividadeDto criarAtividadeValida = new CriarAtividadeDto();
-        ArrayList<Integer> questoes = new ArrayList<>();
-        questoes.add(QUESTAO_UM_VALIDA);
-        questoes.add(QUESTAO_DOIS_VALIDA);
 
         criarAtividadeValida.setDescricao(faker.lorem().paragraph());
         criarAtividadeValida.setEdicaoVemSer("");
         criarAtividadeValida.setPrazoEntrega("2025-03-01T13:01:41.065Z");
-        criarAtividadeValida.setQuestoes(questoes);
+        criarAtividadeValida.setQuestoes(listaIdDuasQuestoesValidasParaAtividade());
         criarAtividadeValida.setTitulo(faker.book().title());
         criarAtividadeValida.setTrilha(Trilha.BACK);
 
@@ -123,6 +122,7 @@ public class CriarAtividadeDataFactory {
     }
 
     public static String atividadeSemPreencherTrilha(){
+        ArrayList<Integer> idQuestoes = listaIdDuasQuestoesValidasParaAtividade();
         return String.format("""
                 {
                     "descricao": "%s",
@@ -135,10 +135,11 @@ public class CriarAtividadeDataFactory {
                     "titulo": "%s",
                     "trilha": ""
                     }
-                """,faker.lorem(), QUESTAO_UM_VALIDA, QUESTAO_DOIS_VALIDA,  faker.book().title());
+                """,faker.lorem(), idQuestoes.get(0), idQuestoes.get(1),  faker.book().title());
     }
 
     public static String atividadePreenchendoTrilhaInvalido(){
+        ArrayList<Integer> idQuestoes = listaIdDuasQuestoesValidasParaAtividade();
         return String.format("""
                 {
                     "descricao": "%s",
@@ -151,19 +152,16 @@ public class CriarAtividadeDataFactory {
                     "titulo": "%s",
                     "trilha": "invalido"
                     }
-                """,faker.lorem(), QUESTAO_UM_VALIDA, QUESTAO_DOIS_VALIDA,  faker.book().title());
+                """,faker.lorem(), idQuestoes.get(0), idQuestoes.get(1), faker.book().title());
     }
 
     public static CriarAtividadeDto atividadeComEdicaoInvalida() {
         CriarAtividadeDto criarAtividadeValida = new CriarAtividadeDto();
-        ArrayList<Integer> questoes = new ArrayList<>();
-        questoes.add(QUESTAO_UM_VALIDA);
-        questoes.add(QUESTAO_DOIS_VALIDA);
 
         criarAtividadeValida.setDescricao(faker.lorem().paragraph());
         criarAtividadeValida.setEdicaoVemSer("Invalido");
         criarAtividadeValida.setPrazoEntrega("2025-03-01T13:01:41.065Z");
-        criarAtividadeValida.setQuestoes(questoes);
+        criarAtividadeValida.setQuestoes(listaIdDuasQuestoesValidasParaAtividade());
         criarAtividadeValida.setTitulo(faker.book().title());
         criarAtividadeValida.setTrilha(Trilha.BACK);
 
