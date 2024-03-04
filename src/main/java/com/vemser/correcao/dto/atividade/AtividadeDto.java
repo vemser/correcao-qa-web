071b6913ff69ed37e417b4ee280353e847773d48
Italo Lacerda <italo.lacerda2017@hotmail.com>
2024-03-04 14:56:23 -0300

    refactor: atualiza dtos de atividade

--- src/main/java/com/vemser/correcao/dto/atividade/AtividadeDto.java
+++ src/main/java/com/vemser/correcao/dto/atividade/AtividadeDto.java
@@ -1,10 +1,13 @@
 package com.vemser.correcao.dto.atividade;
 
+import com.vemser.correcao.dto.questao.QuestaoDto;
 import com.vemser.correcao.enums.Exemplo;
 import com.vemser.correcao.enums.Trilha;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
 
 @Data
 @NoArgsConstructor
@@ -17,5 +20,6 @@ public class AtividadeDto {
     private String userName;
     private Trilha trilha;
     private String prazoEntrega;
+    private ArrayList<QuestaoDto> questoes;
     private Exemplo ativo;
 }
